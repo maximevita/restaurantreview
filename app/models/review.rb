1daef07871629cbb0b9b4e4fc0e5b621c3210893
Maxime Vitaglione <maxime.vitaglione@gmail.com>
2014-11-26 12:18:11 +0100

    added validations to models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  validates :conten, presence: true
+  validates :rating, presence: true
+  validates :rating, format: { with: /\A[0-5]\z/, message: "only allows 0, 1, 2, 3, 4 or 5 !" }
 end
